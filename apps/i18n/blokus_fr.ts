ba5a4ea3a30b504a766c01f02e339bd311c7d0cd
Alexandre Rabérin <kernelith@live.fr>
2021-09-15 12:07:53 +0200

    Update translations.

--- apps/i18n/blokus_fr.ts
+++ apps/i18n/blokus_fr.ts
@@ -24,47 +24,65 @@
         <translation>&amp;Jeu</translation>
     </message>
     <message>
-        <location filename="../MainWindow.ui" line="55"/>
+        <location filename="../MainWindow.ui" line="50"/>
+        <source>&amp;Theme</source>
+        <translation>&amp;Thème</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="63"/>
         <source>&amp;About</source>
         <translation>&amp;A Propos</translation>
     </message>
     <message>
-        <location filename="../MainWindow.ui" line="64"/>
+        <location filename="../MainWindow.ui" line="72"/>
         <source>About &amp;Qt</source>
         <translation>A Propos de &amp;Qt</translation>
     </message>
     <message>
-        <location filename="../MainWindow.ui" line="75"/>
+        <location filename="../MainWindow.ui" line="83"/>
         <source>System language</source>
         <translation>Langue du système</translation>
     </message>
     <message>
-        <location filename="../MainWindow.ui" line="87"/>
+        <location filename="../MainWindow.ui" line="95"/>
         <source>&amp;English</source>
         <translation>&amp;Anglais</translation>
     </message>
     <message>
-        <location filename="../MainWindow.ui" line="99"/>
+        <location filename="../MainWindow.ui" line="107"/>
         <source>&amp;French</source>
         <translation>&amp;Français</translation>
     </message>
     <message>
-        <location filename="../MainWindow.ui" line="108"/>
+        <location filename="../MainWindow.ui" line="116"/>
         <source>&amp;Quit</source>
         <translation>&amp;Fermer</translation>
     </message>
     <message>
-        <location filename="../MainWindow.cpp" line="83"/>
+        <location filename="../MainWindow.ui" line="124"/>
+        <source>&amp;Dark</source>
+        <translation>&amp;Sombre</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="132"/>
+        <source>&amp;Light</source>
+        <translation>&amp;Clair</translation>
+    </message>
+</context>
+<context>
+    <name>blokusUi::MainWindow</name>
+    <message>
+        <location filename="../MainWindow.cpp" line="96"/>
         <source>About</source>
         <translation>A Propos</translation>
     </message>
     <message>
-        <location filename="../MainWindow.cpp" line="85"/>
+        <location filename="../MainWindow.cpp" line="98"/>
         <source>Version: </source>
         <translation>Version : </translation>
     </message>
     <message>
-        <location filename="../MainWindow.cpp" line="86"/>
+        <location filename="../MainWindow.cpp" line="99"/>
         <source>A graphical application for the Blokus game.</source>
         <translation>Une application graphique pour le jeu Blokus.</translation>
     </message>
