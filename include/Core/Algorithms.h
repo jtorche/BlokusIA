33cb9b8266586f0877c9b77a5d02d50f8eaef0c2
Alexandre Rabérin <kernelith@live.fr>
2021-09-22 21:53:26 +0200

    Fix some unwanted copy.

--- include/Core/Algorithms.h
+++ include/Core/Algorithms.h
@@ -11,7 +11,7 @@ namespace core
         std::for_each(
             _begin,
             _end,
-            [&](const auto _p)
+            [&](auto& _p)
             {
                 if (_predicate(_p))
                 {
@@ -21,7 +21,7 @@ namespace core
     }
 
     template<typename Iterable, typename Predicate, typename Operation>
-    void for_each_if(Iterable _iterable, Predicate _predicate, Operation _operation)
+    void for_each_if(Iterable&& _iterable, Predicate _predicate, Operation _operation)
     {
         for_each_if(std::begin(_iterable), std::end(_iterable), _predicate, _operation);
     }
@@ -32,7 +32,7 @@ namespace core
         std::for_each(
             _begin,
             _end,
-            [&](auto _p)
+            [&](auto& _p)
             {
                 auto tp = dynamic_cast<T>(_p);
                 if (tp)
@@ -43,7 +43,7 @@ namespace core
     }
 
     template<typename T, typename Iterable, typename Operation>
-    void dynamic_for_each(Iterable _iterable, Operation _operation)
+    void dynamic_for_each(Iterable&& _iterable, Operation _operation)
     {
         dynamic_for_each<T>(std::begin(_iterable), std::end(_iterable), _operation);
     }
