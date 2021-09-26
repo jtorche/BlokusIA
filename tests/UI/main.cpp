2977c8af1a65168eb0c6cbd4caf0e9bab383c8af
Alexandre Rabérin <kernelith@live.fr>
2021-09-26 19:07:48 +0200

    Basic UI project setup.

---
+++ tests/UI/main.cpp
@@ -0,0 +1,12 @@
+// #include <QApplication>
+
+int main(int , char**)
+// int main(int _argc, char* _argv[])
+{
+    // QApplication app{ _argc, _argv };
+    // app.setApplicationName("Blokus AI Tests");
+    // app.setApplicationVersion("1.0.0");
+
+    // return app.exec();
+    return 0;
+}
